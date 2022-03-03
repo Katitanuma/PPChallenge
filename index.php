43c5ff05e299fdf880f62394b6cd11482a6f6441
Katherin Nuñez <katitanuma@outlook.com>
2022-03-03 03:09:22 -0600

    deploy configuracion

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
+<?php include_once("index.html");
 	header("Access-Control-Allow-Origin: *");
 	header("Access-Control-Allow-Headers: access");
 	header("Access-Control-Allow-Methods: GET,POST");
