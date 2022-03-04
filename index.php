7671b7703e22623405f8552c90b7975cf0c61820
Katherin Nuñez <katitanuma@outlook.com>
2022-03-03 23:30:57 -0600

    prueba paso de datos

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php include_once("index.html");
+<?php
 	/*header("Access-Control-Allow-Origin: *");
 	header("Access-Control-Allow-Headers: access");
 	header("Access-Control-Allow-Methods: GET,POST");
